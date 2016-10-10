8eb5a2bcc32ad50a5f74c2983ea43dfdc93f8a75
IceNeoMax <study4fun2011@gmail.com>
2016-10-10 18:03:42 +0700

    add minichef 1603

--- Minichef/minichef2.js
+++ Minichef/minichef2.js
@@ -2,4 +2,5 @@ $(document).ready(function(){
 
     $("div h1").css('background-color','red');
     $( "#myselect option:selected" ).text();
+    $('body').append("<div><h1>Jquery Core</h1></div>");
 });
