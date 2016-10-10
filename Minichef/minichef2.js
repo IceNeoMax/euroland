224ae21788269bed3d75573abdbb4e79f05ff6d8
IceNeoMax <study4fun2011@gmail.com>
2016-10-10 18:41:26 +0700

    minichef 5

--- Minichef/minichef2.js
+++ Minichef/minichef2.js
@@ -3,4 +3,8 @@ $(document).ready(function(){
     $("div h1").css('background-color','red');
     $( "#myselect option:selected" ).text();
     $('body').append("<div><h1>Jquery Core</h1></div>");
+    $('ul li').remove();
+    $('ul').append('<li><b>0: HTML Tut</b></li><li><b>1: Mongodb Tut</b></li><li><b>2: Python Tut</b></li>');
+    $('#tut').attr("href", "http://www.vnexpress.net/")
+    $('#tut').text("Đọc báo lá cải");
 });
