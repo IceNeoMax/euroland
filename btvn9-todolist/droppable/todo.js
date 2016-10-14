9a65043900320cff507bb23a0f61122a886a2806
IceNeoMax <study4fun2011@gmail.com>
2016-10-14 14:10:11 +0700

    done todo list  droppable

--- btvn9-todolist/droppable/todo.js
+++ btvn9-todolist/droppable/todo.js
@@ -1,3 +1,61 @@
-$( "#sortable1, #sortable2" ).sortable({
-      connectWith: ".connectedSortable"
-    }).disableSelection();
+$(document).ready(function() {
+
+    $('.box-item').draggable({
+        cursor: 'move',
+        helper: "clone"
+    });
+    
+    $('#todoadd').keypress(function (e) {
+        var numtemp = Math.floor(Math.random()*1000000);
+      if (e.which == 13) {
+        var text = $('#todoadd').val();
+        var divx = $("<div itemid=itm-"+numtemp+" class='btn btn-default box-item'/></div>").text(text);
+          $('#container2').append(divx);
+          $( "#container2" ).droppable();
+          $( "#container1, #container2, #container3" ).sortable({
+          connectWith: ".connectedSortable"
+        }).disableSelection();
+          this.value = "";
+      }
+        
+    });
+    
+    
+    
+    
+//connect drag and drop 3 column
+  $("#container1").droppable({
+    drop: function(event, ui) {
+      var itemid = $(event.originalEvent.toElement).attr("itemid");
+      $('.box-item').each(function() {
+        if ($(this).attr("itemid") === itemid) {
+          $(this).appendTo("#container1");
+            $("#container1").find(".btn").attr("disabled",false);
+        }
+      });
+    }
+  });
+
+  $("#container2").droppable({
+    drop: function(event, ui) {
+      var itemid = $(event.originalEvent.toElement).attr("itemid");
+      $('.box-item').each(function() {
+        if ($(this).attr("itemid") === itemid) {
+          $(this).appendTo("#container2");            $("#container2").find(".btn").attr("disabled",false);
+        }
+      });
+    }
+  });
+
+    $("#container3").droppable({
+    drop: function(event, ui) {
+      var itemid = $(event.originalEvent.toElement).attr("itemid");
+      $('.box-item').each(function() {
+        if ($(this).attr("itemid") === itemid) {
+          $(this).appendTo("#container3");           $("#container3").find(".btn").attr("disabled","disabled");
+        }
+      });
+    }
+  });
+
+});
